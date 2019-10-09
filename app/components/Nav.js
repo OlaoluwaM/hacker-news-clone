eb8e61122d904d2cfb3ae1415a5c920ba9556d2f
Olaoluwa Mustapha <jomeemustapha@gmail.com>
2019-10-09 22:26:04 +0100

    Nav Created

---
+++ app/components/Nav.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const activeStyle = {
+  color: 'rgb(187, 46, 31)'
+};
+
+export default class Nav extends React.Component {
+  render() {
+    return (
+      <nav className='row space-between'>
+        <ul className='row nav'>
+          <li>
+            <NavLink
+              activeStyle={activeStyle}
+              className='nav-link'
+              exact
+              to='/'>
+              Top
+            </NavLink>
+          </li>
+          <l>
+            <NavLink activeStyle={activeStyle} className='nav-link' to='/new'>
+              New
+            </NavLink>
+          </l>
+        </ul>
+        <button className='btn-clear'></button>
+      </nav>
+    );
+  }
+}
--- app/index.js
+++ app/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { TopPostDisplay, NewPostDisplay } from './components/Posts';
 import Comment from './components/Comments';
 import User from './components/Users';
+import Nav from './components/Nav';
 
 class App extends React.Component {
   render() {
@@ -12,10 +13,14 @@ class App extends React.Component {
       <Router>
         <div>
           <div className='container'>
-            <Route exact path='/' component={TopPostDisplay} />
-            <Route path='/new' component={NewPostDisplay} />
-            <Route path='/post' component={Comment} />
-            <Route path='/user' component={User} />
+            <Nav />
+            <Switch>
+              <Route exact path='/' component={TopPostDisplay} />
+              <Route path='/new' component={NewPostDisplay} />
+              <Route path='/post' component={Comment} />
+              <Route path='/user' component={User} />
+              <Route render={() => <h1>404</h1>} />
+            </Switch>
           </div>
         </div>
       </Router>
